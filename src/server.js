51552b4668a4f32803d454f2b71782a521db58ff
Gabriel <gabriel58221@gmail.com>
2023-05-10 08:13:55 -0300

    added database

--- src/server.js
+++ src/server.js
@@ -2,6 +2,8 @@ require("express-async-errors");
 
 const AppError = require("./utils/AppError");
 
+const database = require("./database/sqlite")
+
 const express = require('express');
 
 const routes = require("./routes");
@@ -10,6 +12,8 @@ const app = express();
 app.use(express.json());
 
 app.use(routes);
+
+database();
 
 app.use(( error, request, response, next ) => {
     if (error instanceof AppError) {
