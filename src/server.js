cec9b53340e71c44ed58acd60a0cbe80d51fc055
Gabriel Lima <gabriel58221@gmail.com>
2023-07-30 18:23:27 -0300

    a

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 require("express-async-errors");
+require("dotenv/config")
 
 const migrationRun = require("./database/sqlite/migrations")
 const AppError = require("./utils/AppError");
@@ -39,6 +40,7 @@ app.use(( error, request, response, next ) => {
 })
 
 // portal localhost:3330
-const PORT = 3330; 
+
+const PORT = process.env.PORT || 3333;
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
